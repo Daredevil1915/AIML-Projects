a31c8e2653b912c9c6c353e02152fee317a7d8fd
Daredevil1915 <sidgpl45@gmail.com>
2025-08-23 17:16:12 +0530

    fixed interface

--- Linear regression projects/housingapp.py
+++ Linear regression projects/housingapp.py
@@ -8,9 +8,9 @@ model = joblib.load(r"C:\Users\Siddharth Verma\Documents\COURSE\python\Linear re
 
 st.title("🏠 California House Price Prediction")
 
-total_bedrooms=st.number_input("Total Bedrooms",min_value=0.0,format="%.0f")
-households=st.number_input("Households",min_value=1.0,format="%.0f")
-median_income = st.number_input("Median Income", min_value=0.0, format="%.4f")
+total_bedrooms=st.number_input("Total Bedrooms in that area",min_value=0.0,format="%.0f")
+households=st.number_input("Total Households",min_value=1.0,format="%.0f")
+median_income = st.number_input("Median Income of that area", min_value=0.0, format="%.4f")
 housing_median_age = st.number_input("Housing Median Age", min_value=0.0, format="%.0f")
 rooms_per_house = st.number_input("Rooms per House", min_value=0.0, format="%.2f")
 population_per_household = st.number_input("Population per Household", min_value=0.0, format="%.2f")
