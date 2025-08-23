56823c4bf8368f6fc63e14f5766b35a860471d1e
Daredevil1915 <sidgpl45@gmail.com>
2025-08-23 17:24:27 +0530

    added reqtxt

--- Linear regression projects/housingapp.py
+++ Linear regression projects/housingapp.py
@@ -3,18 +3,18 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-preprocessor = joblib.load(r"C:\Users\Siddharth Verma\Documents\COURSE\python\Linear regression projects\preprocessor.joblib")
-model = joblib.load(r"C:\Users\Siddharth Verma\Documents\COURSE\python\Linear regression projects\model.joblib")
+preprocessor=joblib.load(r"C:\Users\Siddharth Verma\Documents\COURSE\python\Linear regression projects\preprocessor.joblib")
+model=joblib.load(r"C:\Users\Siddharth Verma\Documents\COURSE\python\Linear regression projects\model.joblib")
 
 st.title("🏠 California House Price Prediction")
 
 total_bedrooms=st.number_input("Total Bedrooms in that area",min_value=0.0,format="%.0f")
 households=st.number_input("Total Households",min_value=1.0,format="%.0f")
-median_income = st.number_input("Median Income of that area", min_value=0.0, format="%.4f")
-housing_median_age = st.number_input("Housing Median Age", min_value=0.0, format="%.0f")
-rooms_per_house = st.number_input("Rooms per House", min_value=0.0, format="%.2f")
-population_per_household = st.number_input("Population per Household", min_value=0.0, format="%.2f")
-ocean_proximity = st.selectbox("Ocean Proximity", ["<1H OCEAN", "INLAND", "ISLAND", "NEAR BAY", "NEAR OCEAN"])
+median_income=st.number_input("Median Income of that area", min_value=0.0, format="%.4f")
+housing_median_age=st.number_input("Housing Median Age", min_value=0.0, format="%.0f")
+rooms_per_house=st.number_input("Rooms per House", min_value=0.0, format="%.2f")
+population_per_household=st.number_input("Population per Household", min_value=0.0, format="%.2f")
+ocean_proximity=st.selectbox("Ocean Proximity", ["<1H OCEAN", "INLAND", "ISLAND", "NEAR BAY", "NEAR OCEAN"])
 
 median_income=median_income/10000
 
